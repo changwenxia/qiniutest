66f43bbd4002817c70cc9e8178ad7c725cf56736
changwenxia <13522695700@163.com>
2019-11-26 18:07:11 +0800

    feat: 删除配置文件

--- fileConfig.js
+++
@@ -1,8 +0,0 @@
-const fileConfig = {
-  accessKey: 'HPzpKJavfeJqBTVDoIhJFH5jpps_l-ELnEhqSTyx',
-  secretKey: '_NPvXsB2wB3IpOc0NCXgppQPqVv9i52MFOGcv_-r',
-  bucket: 'cwx1',
-  domain: 'http://q09qfa1il.bkt.clouddn.com',
-}
-
-export default fileConfig;
--- src/views/config.js
+++
@@ -1,6 +0,0 @@
-let config = {};
-config.accessKey = 'HPzpKJavfeJqBTVDoIhJFH5jpps_l-ELnEhqSTyx';
-config.secretKey = '_NPvXsB2wB3IpOc0NCXgppQPqVv9i52MFOGcv_-r';
-config.bucket = 'cwx1';  //存储空间的名字
-config.url = 'http://q09qfa1il.bkt.clouddn.com/';  //配置的域名
-module.exports = config;
